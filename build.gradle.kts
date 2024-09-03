b5bcaf9277d15222e98af4de1c90daaf56c39196
주진현 <joojinhyun00@gmail.com>
2024-09-03 19:39:35 +0900

    chore: dokka 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+import org.jetbrains.dokka.DokkaConfiguration
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URL
+
 plugins {
     kotlin("jvm") version "2.0.10"
+    id("org.jetbrains.dokka") version "1.9.20"
 }
 
+description = "Kotlin을 위한 java.time.* 확장 함수 라이브러리"
 group = "io.github.harryjhin"
 version = "1.0.0"
 
@@ -27,4 +33,42 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    outputDirectory.set(file("docs"))
+
+    dokkaSourceSets {
+        configureEach {
+            documentedVisibilities.set(
+                setOf(
+                    DokkaConfiguration.Visibility.PUBLIC,
+                    DokkaConfiguration.Visibility.PROTECTED,
+                )
+            )
+            includes.from(project.files())
+            sourceLink {
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(URL("https://github.com/HarryJhin/slf4j-api-extensions/blob/master/src/main/kotlin"))
+                remoteLineSuffix.set("#L")
+            }
+            samples.from(project.files(), "${rootDir}/src/test/kotlin")
+        }
+    }
+}
+
+tasks.register<Jar>("dokkaHtmlJar") {
+    dependsOn(tasks.dokkaHtml)
+    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+    archiveClassifier.set("html-docs")
+}
+
+tasks.register<Jar>("dokkaJavadocJar") {
+    dependsOn(tasks.dokkaJavadoc)
+    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
+tasks.jar {
+    dependsOn(tasks.named("dokkaJavadocJar"))
 }
